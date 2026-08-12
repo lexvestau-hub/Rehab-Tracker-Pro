513bc11cb80b192b0c577db9d08902a0a582ff87
lexvestau-hub <lexvestau@gmail.com>
2026-08-12 21:46:16 +1000

    Add files via upload

---
+++ Offline Service Worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'rehab-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install event: Caches the initial files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch event: Serves files from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached version if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event: Clears old caches when updated
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
